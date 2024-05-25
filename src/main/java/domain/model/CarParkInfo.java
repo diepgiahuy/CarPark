9467674ce17aa7e135e1db3bb0482571d24bd08b
diepgiahuy <30974088+diepgiahuy@users.noreply.github.com>
2024-05-25 12:09:46 +0700

    first commit

---
+++ src/main/java/domain/model/CarParkInfo.java
@@ -0,0 +1,19 @@
+package domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CarParkInfo {
+    private String carParkNo;
+    private String address;
+    private double latitude;
+    private double longitude;
+    private int totalLots;
+
+}
---
+++ src/test/java/org/example/carpark/CarparkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.carpark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarparkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
